ac9997542a142e13e34cedcbe970f7e1f528df52
adbedada <13406129+adbedada@users.noreply.github.com>
2018-08-01 16:20:38 -0400

    Update gabor_to_vrt.sh

--- gabor_to_vrt.sh
+++ gabor_to_vrt.sh
@@ -1,3 +1,8 @@
+###########################################################################
+# The script creates one vrt file from images stored in different folders
+# June-2018
+###########################################################################
+
 #!/bin/bash
 
 #SBATCH -p 256gb
@@ -14,4 +19,10 @@ export PATH="/groups/engstromgrp/anaconda3/bin:$PATH"
 source activate Ryan_CondaEnvP2.7
 
 # build_vrt
-gdalbuildvrt sri_lanka_all_subsets_gabor.vrt Sri_Lanka_subset_1_gabor/Sri_Lanka_subset_1_gabor/Sri_Lanka_subset_1__BD1_BK1_SC3-5-7_TRgabor/*.tif Sri_Lanka_subset_2_gabor/Sri_Lanka_subset_2_gabor/Sri_Lanka_subset_2__BD1_BK1_SC3-5-7_TRgabor/*.tif Sri_Lanka_subset_3_gabor/Sri_Lanka_subset_3_gabor/Sri_Lanka_subset_3__BD1_BK1_SC3-5-7_TRgabor/*.tif Sri_Lanka_subset_4_gabor/Sri_Lanka_subset_4_gabor/Sri_Lanka_subset_4__BD1_BK1_SC3-5-7_TRgabor/*.tif Sri_Lanka_subset_5_gabor/Sri_Lanka_subset_5_gabor/Sri_Lanka_subset_5__BD1_BK1_SC3-5-7_TRgabor/*.tif Sri_Lanka_subset_6_gabor/Sri_Lanka_subset_6_gabor/Sri_Lanka_subset_6__BD1_BK1_SC3-5-7_TRgabor/*.tif
+gdalbuildvrt sri_lanka_all_subsets_gabor.vrt Sri_Lanka_subset_1_gabor/Sri_Lanka_subset_1_gabor/
+             Sri_Lanka_subset_1__BD1_BK1_SC3-5-7_TRgabor/*.tif Sri_Lanka_subset_2_gabor/Sri_Lanka_subset_2_gabor/
+             Sri_Lanka_subset_2__BD1_BK1_SC3-5-7_TRgabor/*.tif Sri_Lanka_subset_3_gabor/Sri_Lanka_subset_3_gabor/
+             Sri_Lanka_subset_3__BD1_BK1_SC3-5-7_TRgabor/*.tif Sri_Lanka_subset_4_gabor/Sri_Lanka_subset_4_gabor/
+             Sri_Lanka_subset_4__BD1_BK1_SC3-5-7_TRgabor/*.tif Sri_Lanka_subset_5_gabor/Sri_Lanka_subset_5_gabor/
+             Sri_Lanka_subset_5__BD1_BK1_SC3-5-7_TRgabor/*.tif Sri_Lanka_subset_6_gabor/Sri_Lanka_subset_6_gabor/
+             Sri_Lanka_subset_6__BD1_BK1_SC3-5-7_TRgabor/*.tif
